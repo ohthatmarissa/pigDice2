98af8d4d89690b7ffe21ae75cf74e337cf5724f9
Marissa Perry <ohthatmarissa@gmail.com>
2020-02-20 15:08:54 -0500

    add random roll of dice and set all scores to 0 to start

--- app.js
+++ app.js
@@ -11,6 +11,33 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    //get random number 
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //update the round score if number is not 1
+
+
+})
 
 
 
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
