f640f4def017eda7e74bf2c8d2cdeeed91bf5d66
Marissa Perry <ohthatmarissa@gmail.com>
2020-02-28 16:42:48 -0500

    add roundScore functionality and player toggle on 1

--- app.js
+++ app.js
@@ -31,7 +31,27 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     diceDOM.src = 'dice-' + dice + '.png';
 
     //update the round score if number is not 1
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
 
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //this hides the dice after rolling a 1 and changing player
+        document.querySelector('.dice').style.display = 'none';
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+    }
 
 })
 
