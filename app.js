fe20a78754ac87334fee245fddd29222ba38fef5
Marissa Perry <ohthatmarissa@gmail.com>
2020-03-03 14:55:26 -0500

    add hold btn functionality and winner check

--- app.js
+++ app.js
@@ -36,29 +36,57 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        //next player
-        //if active player is 0 then active player must have rolled 1 so then active player gets 0 points
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        //and reset the roundscore to 0
-        roundScore = 0;
+        nextPlayer();
 
-        //each time a new player starts rolling, the roundscore should be 0
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        //this denotes with HTML and CSS .active class, which player is the active roller
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        //this hides the dice after rolling a 1 and changing player
-        document.querySelector('.dice').style.display = 'none';
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
     }
 
 })
 
+//hold points and update interface
+//when the hold button is clicked it will set off this function
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    //add current score(roundScore) to global score 
+    scores[activePlayer] += roundScore;
+
+    //update UI(user interface)
+    //whichever player is active take the score of that player and add to the global score
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+
+    //check if the player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+    } else {
+        //next player
+        nextPlayer();
+    }
+
+
+})
+
+function nextPlayer() {
+    //next player
+    //if active player is 0 then active player must have rolled 1 so then active player gets 0 points
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    //and reset the roundscore to 0
+    roundScore = 0;
+
+    //each time a new player starts rolling, the roundscore should be 0
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    //this denotes with HTML and CSS .active class, which player is the active roller
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    //this hides the dice after rolling a 1 and changing player
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
